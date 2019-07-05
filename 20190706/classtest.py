bf30f871306203c08c46832da355d7cafb3c048e
yaohongdi <yaokevin1987@gmail.com>
2019-07-06 00:52:16 +0800

    property学习注释

--- 20190706/classtest.py
+++ 20190706/classtest.py
@@ -27,3 +27,25 @@ print(cat.age)
 print('deleting operation')
 del cat.age
 print(cat.age)
+
+
+
+'''
+@property 装饰器可以将一个方法变成一个属性来使用，通过 @property 装饰器可以获得和修改对象的某一个属性。
+
+使用 @property 装饰器的方法如下：
+
+只有 @property 表示只读
+同时有 @property 和 @*.setter 表示可读可写
+同时有 @property、@*.setter、和 @*.deleter 表示可读可写可删除
+@property 必须定义在 @*.setter 的前面
+类必须继承 object 父类，否则 @property 不会生效
+
+这样我们就能以访问属性的方式获取和修改 age 属性了。
+
+从这个简单的例子中我们可以发现 age 由一个函数转变为一个属性，并且通过增加一个
+setter 函数的方式来支持 age 的设置。通过 property 和 setter ，可以有效地实现
+get_age（获取对象的属性） 和 set_age（设置对象的属性）这两个操作，而不需要直接将
+内部的 __age 属性暴露出来，同时可以在 setter 函数中对设置的参数进行检查，避免了
+直接对 __age 内部属性进行赋值的潜在风险。
+'''
