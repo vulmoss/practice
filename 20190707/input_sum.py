83f855fc32af910410ef04805fb2fbb479d4fbb8
yaohongdi <yaokevin1987@gmail.com>
2019-07-07 13:13:29 +0800

    file operation introduce

---
+++ 20190707/input_sum.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+a = int(input("please input number a:"))
+b = int(input("please input number b:"))
+c = int(input("please input number c:"))
+m = a + b + c
+print('a is {},b is {},c is {}, a + b + c = {}'.format(a,b,c,m))
